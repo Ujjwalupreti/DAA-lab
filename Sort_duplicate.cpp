458032fbbe68c2c287cc0e8402832bfc854e44c5
Ujjwalupreti <165483596+Ujjwalupreti@users.noreply.github.com>
2025-02-17 10:27:16 +0530

    Add files via upload

---
+++ Sort_duplicate.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool Isduplicate(vector<int> &nums){
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    
+    for(int i=0;i<n;i++){
+        if(nums[i]==nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int n;
+    vector<int> nums;
+    cout<<"Enter the size:";
+    cin>>n;
+    cout<<"Enter the array:"<<endl;
+    for(int i=0;i<n;i++){
+        int c;
+        cin>>c;
+        nums.push_back(c);
+    }
+   
+   cout<<"Duplicate in array:";
+   if(Isduplicate(nums)){
+       cout<<"YES"<<endl;
+   }
+   else{
+       cout<<"NO"<<endl;
+   }
+}
