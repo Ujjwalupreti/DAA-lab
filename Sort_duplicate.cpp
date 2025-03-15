7bf6eda6a5bf022a3004b70f75448bb21534c01e
Ujjwalupreti <165483596+Ujjwalupreti@users.noreply.github.com>
2025-03-15 18:47:04 +0530

    Update Sort_duplicate.cpp

--- Sort_duplicate.cpp
+++ Sort_duplicate.cpp
@@ -5,7 +5,7 @@ bool Isduplicate(vector<int> &nums){
     int n = nums.size();
     sort(nums.begin(),nums.end());
     
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         if(nums[i]==nums[i+1]){
             return true;
         }
